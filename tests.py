285e554c01013e72178b95497a2cdeb2a167c04d
Guilherme Xavier Salles <guilhermexsalles@hotmail.com>
2026-03-26 21:32:34 -0300

    Commit 2: Creating 10 unit tests

--- tests.py
+++ tests.py
@@ -34,3 +34,121 @@ def test_create_choice():
     assert len(question.choices) == 1
     assert choice.text == 'a'
     assert not choice.is_correct
+
+def test_create_question_with_invalid_points_below_minimum():
+    with pytest.raises(Exception):
+        Question(title='q1', points=0)
+
+
+def test_create_question_with_invalid_points_above_maximum():
+    with pytest.raises(Exception):
+        Question(title='q1', points=101)
+
+
+def test_add_choice_returns_created_choice():
+    question = Question(title='q1')
+
+    choice = question.add_choice('Opção A', True)
+
+    assert choice in question.choices
+    assert choice.text == 'Opção A'
+    assert choice.is_correct is True
+
+
+def test_add_multiple_choices_generates_sequential_ids():
+    question = Question(title='q1')
+
+    choice1 = question.add_choice('A')
+    choice2 = question.add_choice('B')
+    choice3 = question.add_choice('C')
+
+    assert choice1.id == 1
+    assert choice2.id == 2
+    assert choice3.id == 3
+
+
+def test_add_choice_with_empty_text_raises_exception():
+    question = Question(title='q1')
+
+    with pytest.raises(Exception):
+        question.add_choice('')
+
+
+def test_add_choice_with_text_longer_than_100_characters_raises_exception():
+    question = Question(title='q1')
+
+    with pytest.raises(Exception):
+        question.add_choice('a' * 101)
+
+
+def test_remove_choice_by_id_removes_only_target_choice():
+    question = Question(title='q1')
+    choice1 = question.add_choice('A')
+    choice2 = question.add_choice('B')
+
+    question.remove_choice_by_id(choice1.id)
+
+    assert len(question.choices) == 1
+    assert question.choices[0].id == choice2.id
+    assert question.choices[0].text == 'B'
+
+
+def test_remove_choice_by_invalid_id_raises_exception():
+    question = Question(title='q1')
+    question.add_choice('A')
+
+    with pytest.raises(Exception):
+        question.remove_choice_by_id(999)
+
+
+def test_remove_all_choices_clears_question_choices():
+    question = Question(title='q1')
+    question.add_choice('A')
+    question.add_choice('B')
+
+    question.remove_all_choices()
+
+    assert question.choices == []
+
+
+def test_set_correct_choices_marks_only_selected_choices():
+    question = Question(title='q1')
+    choice1 = question.add_choice('A')
+    choice2 = question.add_choice('B')
+    choice3 = question.add_choice('C')
+
+    question.set_correct_choices([choice1.id, choice3.id])
+
+    assert choice1.is_correct is True
+    assert choice2.is_correct is False
+    assert choice3.is_correct is True
+
+
+def test_set_correct_choices_with_invalid_id_raises_exception():
+    question = Question(title='q1')
+    question.add_choice('A')
+
+    with pytest.raises(Exception):
+        question.set_correct_choices([999])
+
+
+def test_correct_selected_choices_returns_only_correct_selected_ids():
+    question = Question(title='q1', max_selections=2)
+    choice1 = question.add_choice('A')
+    choice2 = question.add_choice('B')
+    choice3 = question.add_choice('C')
+
+    question.set_correct_choices([choice1.id, choice3.id])
+
+    corrected = question.correct_selected_choices([choice1.id, choice2.id])
+
+    assert corrected == [choice1.id]
+
+
+def test_correct_selected_choices_raises_exception_when_exceeding_max_selections():
+    question = Question(title='q1', max_selections=1)
+    choice1 = question.add_choice('A')
+    choice2 = question.add_choice('B')
+
+    with pytest.raises(Exception):
+        question.correct_selected_choices([choice1.id, choice2.id])
